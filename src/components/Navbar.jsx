1c13b415714c96a150a937ab82f67df409f7e3ae
thenatural86 <philip.smalls86@gmail.com>
2024-06-09 00:28:54 -0300

    adds hero component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { links } from '../data'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  console.log(links)
+  return (
+    <div className='bg-emerald-100'>
+      <div className='mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:item-center sm:py-8'>
+        <h2 className='text-3xl font-bold'>
+          Web <span className='text-emerald-600'>Dev</span>
+        </h2>
+        <div className='flex gap-x-3'>
+          {links.map((link) => {
+            const { id, href, text } = link
+            return (
+              <a
+                key={id}
+                href={href}
+                className='capitalize text-lg tracking-wide hover:text-emerald-600 duration-300'
+              >
+                {text}
+              </a>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
