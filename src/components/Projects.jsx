e0eab39ce375bd7584944a250ec511c23c7a318c
thenatural86 <philip.smalls86@gmail.com>
2024-09-16 10:13:32 -0300

    modify cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <section className='py-20 align-element' id='projects'>
       <SectionTitle text='projects' />
-      <div className='py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8'>
+      <div className='py-16 grid lg:grid-cols-2 xl:grid-cols-2 gap-8'>
         {projects.map((project) => {
           return <ProjectsCard key={project.id} {...project} />
         })}
