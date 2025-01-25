e7e5d82aaba5b6a162043dc1c5b4cbc2cbae409d
thenatural86 <philip.smalls86@gmail.com>
2025-01-25 17:18:45 -0500

    adds anchor tag to img

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -4,11 +4,13 @@ import { TbWorldWww } from 'react-icons/tb'
 const ProjectsCard = ({ url, img, github, title, text }) => {
   return (
     <article className='bg-white rounded-lg shadow-md hover:shadow-xl duration-300'>
-      <img
-        src={img}
-        alt={title}
-        className='w-full object-contain rounded-t-lg h-64 mt-4'
-      />
+      <a href={url} target='_blank'>
+        <img
+          src={img}
+          alt={title}
+          className='w-full object-contain rounded-t-lg h-64 mt-4'
+        />
+      </a>
       <div className='capitalize p-8'>
         <h2 className='text-xl tracking-wide font-medium'>{title}</h2>
         <p className='mt-4 text-slate-700 leading-loose'>{text}</p>
